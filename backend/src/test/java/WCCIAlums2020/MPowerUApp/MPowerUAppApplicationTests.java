fe8bd4995935eb1620c813a6286d8643be1c7246
stella.w.law@gmail.com <stella.w.law@gmail.com>
2021-03-03 18:21:47 -0500

    adding backend

---
+++ backend/src/test/java/WCCIAlums2020/MPowerUApp/MPowerUAppApplicationTests.java
@@ -0,0 +1,13 @@
+package WCCIAlums2020.MPowerUApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPowerUAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
