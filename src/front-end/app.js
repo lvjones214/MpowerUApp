ac784721bd2c145d9ebab5ee14b0c2f3a19f3685
stella.w.law@gmail.com <stella.w.law@gmail.com>
2021-04-06 19:38:30 -0400

    added javascript files

---
+++ src/front-end/app.js
@@ -0,0 +1,24 @@
+import{
+    displayheader
+}from "./js/header.js"
+
+import{
+    displayallCategories
+}from "./js/allCategoriesView.js"
+
+
+import{
+    displaySingleCateogry
+}from "./js/singleCategoryView.js"
+
+import{
+    displaySingleOrganization
+}from "./js/singleOrganizationView.js"
+
+import{
+    displayMain
+}from "./js/main.js"
+
+import{
+    displayFooter
+}from "./js/footer.js"
